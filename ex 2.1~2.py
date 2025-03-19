aa61a08baad4dcc6f92a64169dd079db774a0fc8
jeehongin <jeehongin@gmail.com>
2025-03-19 23:02:03 +0900

    exercise2

---
+++ ex 2.1~2.py
@@ -0,0 +1,13 @@
+Python 3.13.2 (tags/v3.13.2:4f8bb39, Feb  4 2025, 15:23:48) [MSC v.1942 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> #2.1
+>>> print(200, '+', 300, '+', 400, '=', 200+300+400)
+200 + 300 + 400 = 900
+>>> #2.2
+>>> width=30
+>>> height=60
+>>> print(width)
+30
+>>> print(height)
+60
+
