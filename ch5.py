e70c796c24ab3a98233062b3a32742bb83718d17
jeehongin <jeehongin@gmail.com>
2025-04-15 00:45:32 +0900

    week6

--- ch5.py
+++ ch5.py
@@ -1,155 +1,100 @@
-#6.1
-list_ex=[10,20,30,40,50,60,70]
-high=5
-low=3
-print(list_ex[low])
-print(list_ex[low+2])
-print(list_ex[high-low])
-print(list_ex[low-high])
-print(list_ex[-1])
-print(list_ex[-low])
-print(list_ex[2*3])
-print(list_ex[2]*3)
-print(list_ex[5%4])
-print(len(list_ex))
+#5.1
+def my_greet():
+    print('환영합니다.')
 
-#6.3
-list1=[3,5,7]
-list2=[2,3,4,5,6]
+my_greet()
+my_greet()
 
-for i in list1:
-    for j in list2:
-        print('{}*{}={}' .format(i,j,i*j))
-    print('')
+#5.5
+def inch2cm(inch):
+    return inch * 2.54
 
-#6.5
-list1=['I like ','I love ']
-list2=['pancakes.','kiwi juice.','espresso.']
+for i in range(1, 6):
+    cm = inch2cm(i)
+    print(f"{i} 인치 = {cm:.2f} 센티미터")
 
-for i in list1:
-    for j in list2:
-        print('{}' .format(i+j))
-    print('')
+#5.9
+def mean_of_n(nums):
+    return sum(nums) / len(nums)
 
-#6.7
-n_list=[10,20,30,50,60]
-x=0
+def max_of_n(nums):
+    return max(nums)
 
-for i in n_list:
-    x+=i
+def min_of_n(nums):
+    return min(nums)
 
-print('리스트 원소들 :',n_list)
-print('리스트 원소들의 합 :',x)
+input_str = input("정수를 여러 개 입력하시오 : ")
+nums = list(map(int, input_str.split()))
 
-#6.9
-n_list=[10,20,30,50,60]
-m=n_list[0]
+print(f"평균값은 {mean_of_n(nums):.1f}")
+print(f"최댓값은 {max_of_n(nums)}")
+print(f"최솟값은 {min_of_n(nums)}")
 
-for i in n_list:
-    if i>m:
-        m=i
+#5.13
+import math
 
-print('리스트 원소들 :',n_list)
-print('리스트 원소들 중 최댓값 :',m)
+PI = 3.14
 
-#6.11
-n_list=list(map(int,input('5개의 수를 입력하세요: ').split()))
+def cube_volume(s):
+    return s ** 3
 
-sum=0
-for i in n_list:
-    sum+=i
-print('합 :',sum)
+def cuboid_volume(w, h, l):
+    return w * h * l
 
-mean=sum/len(n_list)
-print('평균 :',mean)
+def cone_volume(r, h):
+    return (1/3) * PI * r**2 * h
 
-m=n_list[0]
-for i in n_list:
-    if i>m:
-        m=i
-print('최댓값 :',m)
+def sphere_volume(r):
+    return (4/3) * PI * r**3
 
-n=n_list[0]
-for i in n_list:
-    if i<n:
-        n=i
-        n=i
-print('최솟값 :',n)
+def cylinder_volume(r, h):
+    return PI * r**2 * h
 
-#6.15
-greet='Have a beautiful day.'
-print(greet[0:4])
-print(greet[7:16])
-print(greet[17:20])
+print("모서리 길이가 12인 정육면체:", cube_volume(12))
+print("모서리 길이가 20인 정육면체:", cube_volume(20))
+print("가로, 세로, 깊이가 각각 3, 5, 6인 직육면체:", cuboid_volume(3, 5, 6))
+print("반지름과 높이가 각각 20, 10인 원뿔:", cone_volume(20, 10))
+print("반지름이 15인 구:", sphere_volume(15))
+print("반지름과 높이가 각각 20, 10인 원기둥:", cylinder_volume(20, 10))
 
-#6.17
-animals='dog, cat, tiger, lion'
-animal_list=animals.split(',')
-print('animals=',animal_list)
-print('animals=',animal_list [1:]+animal_list [0:1])
+#5.17
+def sum_range(n1, n2):
+    return sum(range(n1, n2 + 1))
 
-animals=['dog', 'cat', 'tiger', 'lion']
-print('animals =',animals)
+print("{}에서 {}까지의 정수의 합 : {}".format(10, 20, sum_range(10, 20)))
+print("{}에서 {}까지의 정수의 합 : {}".format(40, 100, sum_range(40, 100)))
 
-for animal in animals:
-    print('I love',animal,',')
+#5.21
+def parse_rrn_date(rrn):
+    year = int(rrn[:2])
+    month = int(rrn[2:4])
+    day = int(rrn[4:6])
 
-#6.19
-s_list=['abc', 'bcd', 'abc', 'abba', 'cddc', 'opq', 'opq']
-new_s_list=[]
+    if year >= 50:
+        year += 1900
+    else:
+        year += 2000
 
-for i in s_list:
-    if i not  in new_s_list:
-        new_s_list.append(i)
-print('s_list=',s_list)
-print('new_s_list=',new_s_list)
+    return "{}년 {}월 {}일".format(year, month, day)
 
-#6.21
-src="a2b3c6a2c3f1g1"
-def unzips(a):
-    b=""
-    for i in range(1,len(a),2):
-        for j in range(0,int(a[i])):
-            b+=a[i-1]
-    print(b)
-unzips(src)
+rrn = input("주민등록번호 첫 6숫자 형식 입력 : ")
+print(parse_rrn_date(rrn))
 
-#6.23
-person1=['온달',20,1,180.0,100,0]
-person2=['이사부',25,1,170.0,70.0]
-person3=['평강',22,0,169.0,60.0]
-person4=['혁거세',40,1,150.0,50.0]
+#5.25
+s1 = 'Kang Young Min'
+s2 = 'Kang Young-Min'
+s3 = 'Park Dong Min'
+s4 = ' Park Dong-Yun'
 
-person_list=person1+person3+person4
-def how_many_persons(person_list):
-    return int(len(person_list)/5)
-print(str(how_many_persons(person_list))+'명의 정보가 담겨 있습니다.')
+names = [s1, s2, s3, s4]
 
-person_list=person1+person2+person3+person4
+for s in names:
+    modified = s.replace(" ", "").replace("-", "").upper()
+    print("{}(은)는 {}(으)로 수정됨".format(s.strip(), modified))
+    
+print()
 
-def how_many_persons(person_list):
-    return int(len(person_list)/5)
-
-def compute_average_age(person_list):
-    age = person_list[1::5]
-
-    return sum(ages)/len(ages)
-
-print('평균 나이는',str(compute_average_age(person_list)),'세입니다.')
-
-person_list=person1+person3+person4
-def how_many_persons(a):
-    sex=a[2::5]
-    male=sex.count(1)
-    female=sex.count(0)
-
-    print('리스트에는 남자가 {} 명, 여자가 {} 명입니다.'.format(male,female))
-
-how_many_persons(person_list)
-
-person_list=person1+person3+person4
-def display_persons(a):
-    for i in range(0,len(a),5):
-        print(a[i:i+5])
-
-display_persons(person_list)
+for s in names:
+    modified = s.replace(" ", "").replace("-", "").upper()
+    n_count = modified.count('N')
+    print("{} : {}개의 N이 나타남".format(modified, n_count))
